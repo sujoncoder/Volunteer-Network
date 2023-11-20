074470346080e7832b428dcf30f4a92f00d628ef
sujonphero <sujonsheikh.in@gmail.com>
2023-11-20 22:17:22 +0600

    finioshed login and register page

--- Frontend/src/app/login/page.jsx
+++ Frontend/src/app/login/page.jsx
@@ -1,8 +1,50 @@
-import React from 'react'
+"use client"
+
+import Link from 'next/link';
+import React, { useState } from 'react'
 
 const Login = () => {
+
     return (
-        <div>login</div>
+        <div className="min-h-screen flex items-center justify-center bg-gray-200">
+            <div className="max-w-md w-full p-6 bg-white border rounded-md shadow-md">
+                <h2 className="text-2xl font-semibold mb-6 text-center decoration-wavy decoration-pink-300 underline">Login</h2>
+                <form>
+                    <div className="mb-4">
+                        <label className="block text-gray-700 text-sm font-bold mb-2">
+                            Email
+                        </label>
+                        <input
+                            type="text"
+                            className="border border-gray-300 rounded-md w-full py-2 px-3 focus:outline-none focus:ring focus:border-blue-300"
+                            placeholder="Enter your email"
+                        />
+                    </div>
+                    <div className="mb-4">
+                        <label htmlFor="password" className="block text-gray-700 text-sm font-bold mb-2">
+                            Password
+                        </label>
+                        <input
+                            type="password"
+                            className="border border-gray-300 rounded-md w-full py-2 px-3 focus:outline-none focus:ring focus:border-blue-300"
+                            placeholder="Enter your password"
+                        />
+                    </div>
+
+                    <p className='text-blue-500 hover:underline cursor-pointer flex justify-end text-sm'>
+                        <Link href="/register">
+                            Create an account.
+                        </Link>
+                    </p>
+                    <button
+                        type="button"
+                        className="bg-blue-500 text-white py-2 px-4 rounded-md hover:bg-blue-600 focus:outline-none focus:ring focus:border-blue-300"
+                    >
+                        Login
+                    </button>
+                </form>
+            </div>
+        </div>
     )
 }
 
--- Frontend/src/app/register/page.jsx
+++ Frontend/src/app/register/page.jsx
@@ -1,8 +1,79 @@
-import React from 'react'
+"use client"
+
+import Link from 'next/link';
+import React, { useState } from 'react'
 
 const Register = () => {
+
     return (
-        <div>Register</div>
+        <div className="min-h-screen flex items-center justify-center bg-gray-200">
+            <div className="max-w-md w-full p-6 border bg-white rounded-md shadow-md">
+                <h2 className="text-2xl font-semibold mb-6 text-center">Register as a volunteer</h2>
+                <form>
+                    <div className="mb-4">
+                        <label htmlFor="fullName" className="block text-gray-700 text-sm font-bold mb-2">
+                            Full Name
+                        </label>
+                        <input
+                            type="text"
+                            className="border border-gray-300 rounded-md w-full py-2 px-3 focus:outline-none focus:ring focus:border-blue-300"
+                            placeholder="Enter your full name"
+                        />
+                    </div>
+                    <div className="mb-4">
+                        <label htmlFor="email" className="block text-gray-700 text-sm font-bold mb-2">
+                            Email
+                        </label>
+                        <input
+                            type="email"
+                            className="border border-gray-300 rounded-md w-full py-2 px-3 focus:outline-none focus:ring focus:border-blue-300"
+                            placeholder="Enter your email"
+                        />
+                    </div>
+                    <div className="mb-4">
+                        <label htmlFor="dateOfBirth" className="block text-gray-700 text-sm font-bold mb-2">
+                            Date of Birth
+                        </label>
+                        <input
+                            type="date"
+                            className="border border-gray-300 rounded-md w-full py-2 px-3 focus:outline-none focus:ring focus:border-blue-300"
+                        />
+                    </div>
+                    <div className="mb-4">
+                        <label htmlFor="description" className="block text-gray-700 text-sm font-bold mb-2">
+                            Description
+                        </label>
+                        <textarea
+                            className="border border-gray-300 rounded-md w-full py-2 px-3 focus:outline-none focus:ring focus:border-blue-300"
+                            placeholder="Enter a short description"
+                        />
+                    </div>
+                    <div className="mb-4">
+                        <label className="block text-gray-700 text-sm font-bold mb-2">
+                            Organization
+                        </label>
+                        <input
+                            type="text"
+                            className="border border-gray-300 rounded-md w-full py-2 px-3 focus:outline-none focus:ring focus:border-blue-300"
+                            placeholder="Enter your organization"
+                        />
+                    </div>
+
+                    <p className='text-blue-500 hover:underline cursor-pointer flex justify-end text-sm'>
+                        <Link href="/login">
+                            Already an account.
+                        </Link>
+                    </p>
+
+                    <button
+                        type="submit"
+                        className="bg-blue-500 text-white py-2 px-4 rounded-md hover:bg-blue-600 focus:outline-none focus:ring focus:border-blue-300"
+                    >
+                        Register
+                    </button>
+                </form>
+            </div>
+        </div>
     )
 }
 
