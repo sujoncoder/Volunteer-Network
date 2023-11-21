d83828d63e67b3b29a22a78857d83769c7309d78
moshiur25524 <moshiur.rahman25524@gmail.com>
2023-11-21 18:19:33 +0600

    something modified

--- Frontend/src/app/page.js
+++ Frontend/src/app/page.js
@@ -1,14 +1,79 @@
-import Login from "./login/page";
-import Register from "./register/page";
-import Footer from "./shared/Footer";
-import Navbar from "./shared/Navbar";
+import React from 'react';
+import demo from "../assets/coffee.jpg"
+import Image from 'next/image';
+
 
 export default function Home() {
+  const volunteers = [
+    {
+      img: demo,
+      title: "child protection"
+    },
+    {
+      img: demo,
+      title: "child protection"
+    },
+    {
+      img: demo,
+      title: "child protection"
+    },
+    {
+      img: demo,
+      title: "child protection"
+    },
+    {
+      img: demo,
+      title: "child protection"
+    },
+    {
+      img: demo,
+      title: "child protection"
+    },
+    {
+      img: demo,
+      title: "child protection"
+    },
+    {
+      img: demo,
+      title: "child protection"
+    },
+    {
+      img: demo,
+      title: "child protection"
+    },
+    {
+      img: demo,
+      title: "child protection"
+    }
+  ]
   return (
-    <div>
-      {/* <Navbar /> */}
-      <Login />
-      <Register />
-    </div>
+    <main className='md:mx-[100px]'>
+      <div>
+        <h1 className='uppercase text-2xl font-bolde text-center my-10'>A grow by helping people in need.</h1>
+
+        <div className='flex justify-center items-center space-x-2'>
+          <input className='border border-slate-900 rounded py-2 px-2' type="text" placeholder='search' />
+
+          <button className='py-2 px-4 text-white border rounded bg-blue-500'>Search</button>
+        </div>
+
+        <div className='grid-cols-1 grid sm:grid-cols-3 lg:grid-cols-4 gap-4 my-10'>
+          {
+            volunteers.map((volunteer) => (
+              <div className='bg-gray-100 p-1 rounded '>
+                <Image
+                  src={volunteer.img}
+                  width={1000}
+                  height={1000}
+                  alt='pic'
+                  className='rounded-md w-full h-56 hover:scale-105 duration-500'
+                />
+                <h3 className='text-xl font-bold py-2 px-2'>{volunteer.title}</h3>
+              </div>
+            ))
+          }
+        </div>
+      </div>
+    </main>
   )
 }
--- Frontend/src/app/shared/Navbar.jsx
+++ Frontend/src/app/shared/Navbar.jsx
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 "use client";
 
 import React, { useState } from "react";
@@ -130,6 +131,56 @@ const Navbar = () => {
       </section>
     </main>
   );
+=======
+import React from 'react';
+import Image from 'next/image';
+import Link from 'next/link';
+import logo from "../../assets/logo.png"
+
+const Navbar = () => {
+    return (
+        <nav>
+            <div className="container mx-auto flex items-center justify-between py-4">
+                {/* Left side - Logo */}
+                <div className="flex items-center">
+                    <a href="/">
+                        <Image
+                            src={logo}
+                            width={150}
+                            height={150}
+                            alt='logo'
+                        />
+                    </a>
+                </div>
+
+                {/* Right side - Navigation Links and Buttons */}
+                <div className="flex items-center">
+                    {/* Navigation Links */}
+                    <ul className="flex space-x-4 text-lg">
+                        <li>
+                            <Link href="/" className="font-semibold">Home</Link>
+                        </li>
+                        <li>
+                            <Link href="events" className="font-semibold">Events</Link>
+                        </li>
+                        <li>
+                            <Link href="blog" className="font-semibold">Blogs</Link>
+                        </li>
+                    </ul>
+
+                    {/* Buttons - Login and Dashboard */}
+                    <div className="ml-4">
+                        <Link href="/login">
+                            <button className="bg-green-600 text-white active:bg-green-700 px-4 py-2 rounded-md mr-2">Login</button>
+                        </Link>
+
+                        <button className="bg-blue-500 active:bg-blue-600 text-white px-4 py-2 rounded-md">Dashboard</button>
+                    </div>
+                </div>
+            </div>
+        </nav>
+    );
+>>>>>>> 7ed300ab157ba177673d43642d491da4f6283fff
 };
 
 export default Navbar;
